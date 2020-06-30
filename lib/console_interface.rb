6744f36051582c600a3ecda5c90ff23bcbc724cf
Andrey.M <theambal@gmail.com>
2020-06-30 22:19:47 +0300

    Hangman V1.0

---
+++ lib/console_interface.rb
@@ -0,0 +1,51 @@
+class ConsoleInterface
+
+  FIGURES =
+    Dir[__dir__ + '/../data/figures/*.txt'].
+    sort.
+    map { |file_name| File.read(file_name) }
+
+  def initialize(game)
+    @game = game
+  end
+
+  def print_out
+    print <<~END
+        Слово: #{word_to_show}
+        #{figure}
+        Ошибки (#{@game.errors_made}): #{errors_to_show}
+        У вас осталось ошибок: #{@game.errors_allowed}
+        Введите следующую букву:
+        END
+      if @game.won?
+        puts 'Поздравляю вы выиграли!'
+      elsif @game.lost?
+        puts "Вы проиграли, загаданное слово: #{@game.word}"
+      end
+  end
+
+  def figure
+    FIGURES[@game.errors_made]
+  end
+
+  def word_to_show
+    result =
+      @game.letters_to_guess.map do |letter|
+      if letter == nil
+        "__"
+      else
+        letter
+      end
+    end
+    result.join(' ')
+  end
+
+  def errors_to_show
+    @game.errors.join(', ')
+  end
+
+  def get_input
+    print 'Следующая буква: '
+    letter = gets[0].upcase
+  end
+end
---
+++ lib/game.rb
@@ -0,0 +1,71 @@
+class Game
+
+  TOTAL_ERRORS_ALLOWED = 7
+
+  def initialize(word)
+    @letters = word.chars
+    @user_guesses = []
+  end
+
+  def errors
+    @user_guesses - normalize_letters
+  end
+
+  def errors_made
+    errors.length
+  end
+
+  def errors_allowed
+    TOTAL_ERRORS_ALLOWED - errors_made
+  end
+
+  def letters_to_guess
+    result =
+      @letters.map do |letter|
+        if @user_guesses.include?(normalize_letter(letter))
+          letter
+        else
+          nil
+        end
+      end
+    result
+  end
+
+  def over?
+    won? || lost?
+  end
+
+  def play!(letter)
+    if !over? && !@user_guesses.include?(normalize_letter(letter))
+      @user_guesses << letter
+    end
+  end
+
+  def won?
+    (normalize_letters - @user_guesses).empty?
+  end
+
+  def lost?
+    errors_allowed == 0
+  end
+
+  def word
+    @letters.join
+  end
+
+  def normalize_letter(letter)
+    result =
+      if letter == 'Ё'
+        'Е'
+      elsif letter == 'Й'
+        'И'
+      else
+        letter
+      end
+    result
+  end
+
+  def normalize_letters
+    @letters.map { |letter| normalize_letter(letter) }
+  end
+end
